e03d3dd1ce9df5b0a68a72ebd792384a241bf8d3
Nick Woodward <nickwoodwrd@gmail.com>
2015-11-29 19:02:12 -0600

    9 - Fix implementations for post, put, and patch * Added usages of the new parameters to the requests.

--- src/hateoas-interceptor/hateoas-interceptor.service.js
+++ src/hateoas-interceptor/hateoas-interceptor.service.js
@@ -41,19 +41,19 @@ export default function hateoasInterceptor($hateoasConfig, $q, $injector) {
 
             defineHiddenProperty(data, '$post', (key, data, config) => {
                 return data.$link(key).then((link) => {
-                    return $http.post(link, config);
+                    return $http.post(link, data, config);
                 });
             });
 
             defineHiddenProperty(data, '$put', (key, data, config) => {
                 return data.$link(key).then((link) => {
-                    return $http.put(link, config);
+                    return $http.put(link, data, config);
                 });
             });
 
             defineHiddenProperty(data, '$patch', (key, data, config) => {
                 return data.$link(key).then((link) => {
-                    return $http.patch(link, config);
+                    return $http.patch(link, data, config);
                 });
             });
 
