38df626d247b0355b87b14a9ecc5df7db7b791c1
Nick Woodward <nick@salte.io>
2016-10-22 01:32:23 -0500

    fix: implement semantic release (#22)
    * Integrated Sauce Labs
    * Updated packages
    * Updated travis config to run quicker
    * Added tests for hateoas-config
    
    closes #21 and closes #20

--- src/hateoas-interceptor/hateoas-interceptor.module.js
+++ src/hateoas-interceptor/hateoas-interceptor.module.js
@@ -1,5 +1,5 @@
 import hateoasInterceptor from './hateoas-interceptor.service';
 
-var module = angular.module('core.services.hateoas-interceptor', []).service('hateoasInterceptor', hateoasInterceptor);
+const module = angular.module('core.services.hateoas-interceptor', []).service('hateoasInterceptor', hateoasInterceptor);
 
 export default module.name;
